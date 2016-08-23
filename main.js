8b327ce8d30684a6e093ec253bf012f39201f701
Mykheievskyi <mykheievskyi@gmail.com>
2016-08-23 13:21:02 +0300

    correct data convert

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 $(function(){
-	/* выбор города */
-	$('.delivery_list').click(function(){
-		$(".cities_list").slideToggle('fast');
-	});
-	$('ul.cities_list li').click(function(){
-		var tx = $(this).html();
-		var tv = $(this).attr('alt');
-		$(".cities_list").slideUp('fast');
-		$(".delivery_list span").html(tx);
-	});
-})
+ /* выбор города */
+ $('.delivery_list').click(function(){
+ $(".cities_list").slideToggle('fast');
+ });
+ $('ul.cities_list li').click(function(){
+ var tx = $(this).html();
+ var tv = $(this).attr('alt');
+ $(".cities_list").slideUp('fast');
+ $(".delivery_list span").html(tx);
+ $(".delivery_text").html(tv);
+ });
+ })
 
 
 var APIKEY  = 'e9fc0dfa8729f1e73c92f579cce35aea';
@@ -23,72 +24,77 @@ xhttp.onreadystatechange = function() {
 
 		var weather = JSON.parse(xhttp.responseText);
 		
-		var country = weather.sys.country; 		// 	Страна
-		var sunrice = weather.sys.sunrise; 		// Время рассвета
-		var sunset = weather.sys.sunset;			// Время заката
-		var cloud = weather.clouds.all;				// Облачность
-		var temp = weather.main.temp - 273;		// Температура (в Цельсиях)
-		var pressure = weather.main.pressure;	// Давление
-		var maxTemp = weather.main.temp_max;	// Максимальная температура
-		var minTemp = weather.main.temp_min;	// Минимальная температура
-		var speedWind = weather.wind.speed;		// Скорость ветра
-		var degWind = weather.wind.deg; 			// Направление ветра
+		
+		var country = weather.sys.country; 													// Страна
+		var sunrice = weather.sys.sunrise; 													// Время рассвета
+		var sunset = weather.sys.sunset;														// Время заката
+		var cloud = weather.clouds.all;															// Облачность
+		var temp =  Math.floor(weather.main.temp - 273);						// Температура (в Цельсиях)
+		var pressure = weather.main.pressure;												// Давление
+		var maxTemp =  Math.floor(weather.main.temp_max - 273);			// Максимальная температура
+		var minTemp =  Math.floor(weather.main.temp_min - 273);			// Минимальная температура
+		var speedWind = weather.wind.speed;													// Скорость ветра
+		var degWind = weather.wind.deg; 														// Направление ветра
 
 		var newTable = document.createElement('table');
 
-		newTable.id = 't1';
-		newTable.width = 500;
+		newTable.id = 't';
+		newTable.width = 800;
+		newTable.border = 1;
 
 		var titleRow = newTable.insertRow(0);
-		titleRow.id = 'tr';
-		var time = new Date();
+		var valueRow = newTable.insertRow(1);
+		titleRow.id = 'tirle';
+		valueRow.id = 'value';
+		
+		function corectTime(t)
+		{
+				return t<10 ? "0"+t : t;
+		}
+		
+		titleRow.insertCell().innerHTML = 'Страна'
+		valueRow.insertCell().innerHTML = country;
 
-		var countryCell = titleRow.insertCell();
-		countryCell.innerHTML = country;
+		
+		titleRow.insertCell().innerHTML = 'Время рассвета';
+		var timeSunrice= new Date(sunrice*1000);
+		valueRow.insertCell().innerHTML = corectTime(timeSunrice.getHours() )+':'+corectTime(timeSunrice.getMinutes())+':'+corectTime(timeSunrice.getSeconds());
 
-		var sunriceCell = titleRow.insertCell();
+		titleRow.insertCell().innerHTML = 'Время заката';
+		var timeSunset= new Date(sunset*1000);
+		valueRow.insertCell().innerHTML = corectTime(timeSunset.getHours())+':'+corectTime(timeSunset.getMinutes())+':'+corectTime(timeSunset.getSeconds());
 		
-		sunriceCell.innerHTML = sunrice;
+		titleRow.insertCell().innerHTML = 'Облачность';
+		valueRow.insertCell().innerHTML = cloud;
 
-		var sunsetCell = titleRow.insertCell();
-		
-		sunsetCell.innerHTML = sunset;
-		
-		var cloudCell = titleRow.insertCell();
-		
-		cloudCell.innerHTML = cloud;
+		titleRow.insertCell().innerHTML = 'Температура °C';
+		valueRow.insertCell().innerHTML =  temp;
 
-		var tempCell = titleRow.insertCell();
-		
-		tempCell.innerHTML = temp;
+		titleRow.insertCell().innerHTML = 'Давление';
+		valueRow.insertCell().innerHTML = pressure;
 
-		var pressureCell = titleRow.insertCell();
-		
-		pressureCell.innerHTML = pressure;
+		titleRow.insertCell().innerHTML = 'Максимальная температура';
+		valueRow.insertCell().innerHTML =  maxTemp;
 
-		var maxTempCell = titleRow.insertCell();
-		
-		maxTempCell.innerHTML = maxTemp;
+		titleRow.insertCell().innerHTML = 'Минимальная температура';
+		valueRow.insertCell().innerHTML =  minTemp;
 
-		var minTempCell = titleRow.insertCell();
-		
-		minTempCell.innerHTML = minTemp;
+		titleRow.insertCell().innerHTML = 'Скорость ветра';
+		valueRow.insertCell().innerHTML = speedWind;
 
-		var speedWindCell = titleRow.insertCell();
-		
-		speedWindCell.innerHTML = speedWind;
+		titleRow.insertCell().innerHTML = 'Направление ветра ↑';
+		valueRow.insertCell().innerHTML =  degWind;
 
-		var degWindCell = titleRow.insertCell();
-		
-		degWindCell.innerHTML = degWind;
 
 		document.body.appendChild(newTable);
+	
 
-
-	}
+}
 };
 xhttp.open("GET", fullURL, true);
 xhttp.send();
+
+
 
 
 
